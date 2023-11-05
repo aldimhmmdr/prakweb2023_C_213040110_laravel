bbb8045982df37473535aa0f93cca3c1db27a8c1
aldimhmmdr <ramdani.aldi93@gmail.com>
2023-11-05 21:32:54 +0700

    9. Database Seeder

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,63 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Aldi Muhammad',
+            'email' => 'aldimuhammad@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Dika Karomalloh',
+            'email' => 'dika@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'web programing',
+            'slug' => 'web-programing'
+        ]);
+
+        Category::create([
+            'name' => 'personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+            'body' => '  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo culpa architecto deleniti at! Optio provident beatae, nam veniam ullam blanditiis asperiores dolor totam obcaecati commodi delectus quia, dignissimos possimus minima eum? Earum rem porro, quo molestias sit placeat dolore odio ad minus nihil distinctio expedita voluptates illo deleniti praesentium maiores modi aperiam alias facere? Praesentium in, dolores placeat sit vel esse, cum magni optio quisquam harum impedit repudiandae deleniti obcaecati aliquam rerum quam sunt magnam nobis odio! Nobis hic voluptate, molestias minima eaque ut unde magnam, veniam consectetur itaque praesentium ipsa eos, nesciunt expedita possimus quas fugiat inventore nulla repudiandae.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+            'body' => '  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo culpa architecto deleniti at! Optio provident beatae, nam veniam ullam blanditiis asperiores dolor totam obcaecati commodi delectus quia, dignissimos possimus minima eum? Earum rem porro, quo molestias sit placeat dolore odio ad minus nihil distinctio expedita voluptates illo deleniti praesentium maiores modi aperiam alias facere? Praesentium in, dolores placeat sit vel esse, cum magni optio quisquam harum impedit repudiandae deleniti obcaecati aliquam rerum quam sunt magnam nobis odio! Nobis hic voluptate, molestias minima eaque ut unde magnam, veniam consectetur itaque praesentium ipsa eos, nesciunt expedita possimus quas fugiat inventore nulla repudiandae.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+            'body' => '  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo culpa architecto deleniti at! Optio provident beatae, nam veniam ullam blanditiis asperiores dolor totam obcaecati commodi delectus quia, dignissimos possimus minima eum? Earum rem porro, quo molestias sit placeat dolore odio ad minus nihil distinctio expedita voluptates illo deleniti praesentium maiores modi aperiam alias facere? Praesentium in, dolores placeat sit vel esse, cum magni optio quisquam harum impedit repudiandae deleniti obcaecati aliquam rerum quam sunt magnam nobis odio! Nobis hic voluptate, molestias minima eaque ut unde magnam, veniam consectetur itaque praesentium ipsa eos, nesciunt expedita possimus quas fugiat inventore nulla repudiandae.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+            'body' => '  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo culpa architecto deleniti at! Optio provident beatae, nam veniam ullam blanditiis asperiores dolor totam obcaecati commodi delectus quia, dignissimos possimus minima eum? Earum rem porro, quo molestias sit placeat dolore odio ad minus nihil distinctio expedita voluptates illo deleniti praesentium maiores modi aperiam alias facere? Praesentium in, dolores placeat sit vel esse, cum magni optio quisquam harum impedit repudiandae deleniti obcaecati aliquam rerum quam sunt magnam nobis odio! Nobis hic voluptate, molestias minima eaque ut unde magnam, veniam consectetur itaque praesentium ipsa eos, nesciunt expedita possimus quas fugiat inventore nulla repudiandae.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
