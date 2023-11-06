9ccb23ef664f4b02a655c1d856d39b5fa422f7ef
aldimhmmdr <ramdani.aldi93@gmail.com>
2023-11-06 21:12:13 +0700

    13.Searching & Pagination

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -34,28 +34,13 @@ Route::get('/about', function () {
         "image" => "aldi.jpg"
     ]);
 });
-Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', function () {
     return view('categories', [
-        'title' => 'Post Categories',
+        'title' => "Post by Categories",
         'active' => 'categories',
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'title' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author'),
-    ]);
-});
-
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
